2e8ed3e6f75ec15b8125ad329a5b052872144b4b
Stefan Seelmann <mail@stefan-seelmann.de>
2016-03-13 22:23:10 +0100

    Jenkins

--- roles/jenkins/files/seed.groovy
+++ roles/jenkins/files/seed.groovy
@@ -1,11 +1,24 @@
 mavenJob('directory-shared-generated') {
     logRotator(-1, 100)
     scm {
-        github('apache/directory-shared', 'master')
+        github('apache/directory-shared', 'trunk')
     }
     triggers {
         scm('@daily')
     }
     rootPOM('pom.xml')
-    goals('clean install')
+    goals('-V clean install')
 }
+
+mavenJob('directory-server-generated') {
+    logRotator(-1, 100)
+    scm {
+        github('apache/directory-server', 'trunk')
+    }
+    triggers {
+        scm('@daily')
+    }
+    rootPOM('pom.xml')
+    goals('-V clean install')
+}
+
